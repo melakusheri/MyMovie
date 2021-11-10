121b6ce28e5626dbc66e9b891b2246164ed3937d
Melaku Sheri <mailangels@gmail.com>
2021-11-10 17:01:57 -0500

    confi and app Adjuted

---
+++ client/src/components/Config.js
@@ -0,0 +1,14 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'de7ae1640aef4f42680a3720a1867d00';
+
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -18,7 +18,7 @@ function NavBar() {
   return (
     <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
       <div className="menu__logo">
-        <a style={{ color: '#BD0A28', fontWeight: 'bolder' }} href="/">TBP Project</a>
+        <a style={{ color: '#BD0A28', fontWeight: 'bolder' }} href="/">My Movies</a>
       </div>
       <div className="menu__container">
         <div className="menu_left">
